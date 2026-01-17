3010a5587f442452954c24fdab453f84e050a4d6
osbertodiaz <osbertodiaz@gmail.com>
2026-01-17 16:13:27 +0100

    Fixed App.tsx for reference error

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App: React.FC = () => {
               path="projects"
               element={
                 <RequireRole allowedRoles={routeRoles["/projects"]}>
-                  <ProjectsPage />
+                  <ProjectListPage />
                 </RequireRole>
               }
             />
